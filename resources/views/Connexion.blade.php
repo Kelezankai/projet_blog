25ba534794481f4914b2397a0b70e3c04e963a8a
SINALY I DOUMBIA <Kelezankai.sinalyibrahima@gmail.com>
2025-04-12 15:29:53 +0000

    Message

--- resources/views/Connexion.blade.php
+++ resources/views/Connexion.blade.php
@@ -7,45 +7,6 @@
     <title>COLLECTIF DES ETUDIANTS POUR LE CHANGEMENT</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
-        <header>
-            <h2 class="logo">Logo </h2>
-            <nav class="navigation">
-                <a href="#">Accueil</a>
-                <a href="#">à propos</a>
-                <a href="#">Contacts</a>
-                <button  class="btnLogin-popup">Login</button>
-            </nav>
-        </header>
-          <img class="fond_page" href="C:\Users\HP\Desktop\C.E.C\Collectif.jpeg">
-
-        <div class="wrapper">
-            <!-- <span class="icon-close">
-                <ion-icon name="close-outline"></ion-icon>
-            </span> -->
-           <div class="form-box Login">
-            <h2>Login</h2>
-            <form method="post" action="Connexion">
-                <div class="input-box">
-                    <span class="icon"><ion-icon name="mail"></ion-icon></span>
-                    <input type="email" required>
-                    <label>Email</label>
-                </div>
-                <div class="input-box">
-                    <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
-                    <input type="password" required>
-                    <label>mot de passe</label>
-                </div>
-                <!-- <div class="remember-forgot">
-                    <label><input type="checkbox">Remember me</label>
-                    <a href="#">Forgot password?</a>
-                </div> -->
-                <button type="submit"  class="btn">Login</button>
-                <div class="Login-register">
-                    <p>Don't have an account ?<a href="Inscription" class="register-link">Register</a></p>
-                </div>
-            </form>
-           </div> 
-
     <header>
         <h2 class="logo">Logo </h2>
         <nav class="navigation">
@@ -56,40 +17,37 @@
             <button class="btnLogin-popup">Login</button>
         </nav>
     </header>
-    <img class="fond_page" href="C:\Users\HP\Desktop\C.E.C\Collectif.jpeg">
+  
 
     <div class="wrapper">
-        <span class="icon-close">
-            <ion-icon name="close-outline"></ion-icon>
-        </span>
-    </div>
-    <div class="form-box Login">
-        <h2>Login</h2>
-    </div>
-    <div>
-        <form method="post" action="traitement.php">
-            <div class="input-box">
+
+        <div class="form-box Login">
+           <h2>Login</h2>
+
+          <form action="{{ url('livres/Connexion') }}" method="POST">
+            @csrf
+           <div class="input-box">
                 <span class="icon"><ion-icon name="mail"></ion-icon></span>
                 <input type="email" required>
                 <label>Email</label>
-            </div>
-            <div class="input-box">
-                <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+           </div>
+ 
+          <div class="input-box">
                 <input type="password" required>
                 <label>mot de passe</label>
-            </div>
-            <div class="remember-forgot">
-                <label><input type="checkbox">Remember me</label>
-                <a href="#">Forgot password?</a>
-            </div>
-            <button type="submit" class="btn">Login</button>
-            <div class="Login-register">
-                <p>Don't have an account ?<a href="formulaire make my persona.html" class="register-link">Register</a></p>
-            </div>
-        </form>
+           </div>
+           <button type="submit" class="btn">Se connecter</button>
+           <div class="Login-register">
+               <p>Don't have an account ?<a href="Inscription" class="register-link">Inscription</a></p>
+           </div>
+          </form>
+            @csrf
+       </div>
+
+
+   
     </div>
-
-
+   
 
 
 
